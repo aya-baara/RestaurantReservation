0254cbef782cab9c092b4d4e8c5ac68543eb9ddc
ayabaara <ayabaara4@gmail.com>
2025-05-05 11:28:19 +0300

    Add Employee  model

---
+++ RestaurantReservation.Db/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantReservation.Db.Models;
+
+class Employee
+{
+    public int EmployeeId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+    public int ResturantId { get; set; }
+    public Restaurant Restaurant { get; set; }
+}
+
