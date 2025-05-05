419d53045d0783a9a85c2b30b8dc761b18b993ac
ayabaara <ayabaara4@gmail.com>
2025-05-05 14:46:53 +0300

    generate the migration for creating tables

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using RestaurantReservation.Db;
+using Microsoft.EntityFrameworkCore;
+
+//var host = Host.CreateDefaultBuilder()
+//    .ConfigureAppConfiguration((context, config) =>
+//    {
+//        config.AddJsonFile("appsettings.json");
+//    })
+//    .ConfigureServices((context, services) =>
+//    {
+//        services.AddDbContext<RestaurantReservationDbContext>(options =>
+//            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
+//    })
+//    .Build();
+
+//using (var scope = host.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<RestaurantReservationDbContext>();
+//    Console.WriteLine("DB Context is ready.");
+//}
