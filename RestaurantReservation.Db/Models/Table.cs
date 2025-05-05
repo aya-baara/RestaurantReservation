5bc8ebbc9a61e53bb2311e394341786cf99b5823
ayabaara <ayabaara4@gmail.com>
2025-05-05 11:34:17 +0300

    Add table mpdel

---
+++ RestaurantReservation.Db/Models/Table.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservation.Db.Models;
+
+class Table
+{
+    public int TableId { get; set; }
+    public int Capacity { get; set; }
+
+    public int ResturantId { get; set; }
+    public Restaurant Restaurant { get; set; }
+}
+
