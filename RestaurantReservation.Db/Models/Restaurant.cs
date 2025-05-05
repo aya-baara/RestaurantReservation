df9a65f6ec5cc431216f3706ab4b04926b4a96b2
ayabaara <ayabaara4@gmail.com>
2025-05-05 11:17:41 +0300

    Adding  Restaurant model

---
+++ RestaurantReservation.Db/Models/Restaurant.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservation.Db.Models;
+
+public class Restaurant
+{
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public int PhoneNumber { get; set; }
+    public string OpeningHours { get; set; }
+}
+
