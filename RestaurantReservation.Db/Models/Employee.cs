0f0334e248c1fe2004c63faf179e20a242e4c5c5
ayabaara <ayabaara4@gmail.com>
2025-05-05 11:40:17 +0300

    Add order model & Edit Emplyee to use it

--- RestaurantReservation.Db/Models/Employee.cs
+++ RestaurantReservation.Db/Models/Employee.cs
@@ -8,5 +8,6 @@ class Employee
     public string Position { get; set; }
     public int ResturantId { get; set; }
     public Restaurant Restaurant { get; set; }
+    public List<Order> Orders { get; set; } = new List<Order>();
 }
 
---
+++ RestaurantReservation.Db/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservation.Db.Models;
+
+class Order
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public int TotalAmount { get; set; }
+    public int EmployeeId { get; set; }
+    public Employee Employee { get; set; }
+}
+
