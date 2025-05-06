6aafa91268695fabe39a6c87ef7769cd91b96415
ayabaara <ayabaara4@gmail.com>
2025-05-06 16:09:18 +0300

    implement ListManagers method

--- RestaurantReservation.Db/Repository/EmployeeRepository.cs
+++ RestaurantReservation.Db/Repository/EmployeeRepository.cs
@@ -1,12 +1,22 @@
-﻿using RestaurantReservation.Db.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Interface;
 using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Models.Enums;
 
 namespace RestaurantReservation.Db.Repository;
 
 public class EmployeeRepository : Repository<Employee>, IRepository<Employee>
 {
+    private readonly RestaurantReservationDbContext _context;
     public EmployeeRepository(RestaurantReservationDbContext restaurantReservationDbContext) : base(restaurantReservationDbContext)
     {
+        _context = restaurantReservationDbContext;
+    }
+
+    public async Task<List<Employee>> ListManagers()
+    {
+        return await
+            _context.Employees.Where(e => e.Position.Equals(EmployeePosition.Manager)).ToListAsync();
     }
 }
 
