4b843c3c20ae66bb6484c135ceb1b4d454c3992c
ayabaara <ayabaara4@gmail.com>
2025-05-06 11:48:07 +0300

    Create Enum for Employee Position and use it

--- RestaurantReservation.Db/Models/Employee.cs
+++ RestaurantReservation.Db/Models/Employee.cs
@@ -1,11 +1,13 @@
-﻿namespace RestaurantReservation.Db.Models;
+﻿using RestaurantReservation.Db.Models.Enums;
+
+namespace RestaurantReservation.Db.Models;
 
 public class Employee
 {
     public int EmployeeId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Position { get; set; }
+    public EmployeePosition Position { get; set; }
     public int RestaurantId { get; set; }
     public Restaurant Restaurant { get; set; }
     public List<Order> Orders { get; set; } = new List<Order>();
