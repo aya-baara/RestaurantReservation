a392d8a5d231bb0bababc2158c1b585b527f5c8f
ayabaara <ayabaara4@gmail.com>
2025-05-06 13:37:15 +0300

    Implement CutomerRepo and test functions in console

---
+++ RestaurantReservation.Db/Repository/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantReservation.Db.Interface;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Repository;
+
+public class CustomerRepository : Repository<Customer>, IRepository<Customer>
+{
+    public CustomerRepository(RestaurantReservationDbContext restaurantReservationDbContext) : base(restaurantReservationDbContext)
+    {
+    }
+}
+
--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,23 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿
+using RestaurantReservation.Db.Models;
 using RestaurantReservation.Db;
-using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Repository;
 
-//var host = Host.CreateDefaultBuilder()
-//    .ConfigureAppConfiguration((context, config) =>
-//    {
-//        config.AddJsonFile("appsettings.json");
-//    })
-//    .ConfigureServices((context, services) =>
-//    {
-//        services.AddDbContext<RestaurantReservationDbContext>(options =>
-//            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
-//    })
-//    .Build();
 
-//using (var scope = host.Services.CreateScope())
-//{
-//    var db = scope.ServiceProvider.GetRequiredService<RestaurantReservationDbContext>();
-//    Console.WriteLine("DB Context is ready.");
-//}
+
+var context = new RestaurantReservationDbContext();
+var customerRepo = new CustomerRepository(context);
+
+var newCustomer = new Customer { FirstName = "Ahmad", LastName = "Mahmoud", Email = "Ahmad@gmail.com", PhoneNumber = 85456464 };
+await customerRepo.Create(newCustomer);
+
+Customer customer = customerRepo.GetById(3).Result;
+
+customer.Email = "ayabaara4@gmail.com";
+await customerRepo.Update(customer);
+
+await customerRepo.DeleteById(6);
+
+Console.WriteLine( customerRepo.GetById(6));
