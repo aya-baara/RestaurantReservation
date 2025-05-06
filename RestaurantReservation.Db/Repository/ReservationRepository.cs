1dd1307a99d6a356463dec71ea5e6af7c8699c02
ayabaara <ayabaara4@gmail.com>
2025-05-06 16:22:53 +0300

    implement GetReservationsByCustomer method in the Reservation Repo

--- RestaurantReservation.Db/Repository/ReservationRepository.cs
+++ RestaurantReservation.Db/Repository/ReservationRepository.cs
@@ -1,12 +1,21 @@
-﻿using RestaurantReservation.Db.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Interface;
 using RestaurantReservation.Db.Models;
 
 namespace RestaurantReservation.Db.Repository;
 
 public class ReservationRepository : Repository<Reservation>, IRepository<Reservation>
 {
+    private readonly RestaurantReservationDbContext _context;
     public ReservationRepository(RestaurantReservationDbContext restaurantReservationDbContext) : base(restaurantReservationDbContext)
     {
+        _context = restaurantReservationDbContext;
+    }
+    public async Task<List<Reservation>> GetReservationsByCustomer(int csustomerId)
+    {
+        return await
+            _context.Reservations.Where(r => r.CustomerId == csustomerId).ToListAsync();
+
     }
 }
 
