7a8f8c51551ff0f9c747dfe221b8305059eba2ee
ayabaara <ayabaara4@gmail.com>
2025-05-07 08:23:01 +0300

    generate migration for creating GetTotalRevenueByRestaurant function

---
+++ RestaurantReservation.Db/Migrations/20250507043644_AddGetTotalRevenueByRestaurantFunction.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetTotalRevenueByRestaurantFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        CREATE FUNCTION dbo.TotalRevenue
+            (@RestuarantId Int)
+            RETURNS Int
+            AS
+            BEGIN
+                return (
+	            select sum(o.TotalAmount) from Reservations r
+	            join Orders o on r.ReservationId = o.ReservationId
+	            where r.RestaurantId = @RestuarantId
+	            )
+            END
+         ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS TotalRevenue");
+        }
+
+    }
+}
