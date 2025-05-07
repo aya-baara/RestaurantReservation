25810814e4ed3a82c02f22d3b4fcf9896fbafaaa
ayabaara <ayabaara4@gmail.com>
2025-05-07 07:21:14 +0300

    generate migration for EmployeeDetails view

---
+++ RestaurantReservation.Db/Migrations/20250507035644_AddEmployeeDetailsView.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeeDetailsView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        CREATE VIEW EmployeeDetails AS
+        SELECT e.FirstName,e.LastName ,e.Position , res.Name ,res.Address
+        FROM Employees e
+        join Restaurants res on e.RestaurantId= res.RestaurantId;
+        ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW IF EXISTS EmployeeDetails;");
+        }
+    }
+}
