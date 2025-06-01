bd127114dfce66559a8ce49478dd02df3b06d7df
ayabaara <ayabaara4@gmail.com>
2025-06-01 11:01:12 +0300

    add customerDto model

---
+++ RestaurantReservation.API/Models/CustomerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservation.API.Models;
+
+public class CustomerDto
+{
+    public int CustomerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
+
