7d6b10e5fe39ac29bbc9ef0440ced5c132994b0a
ayabaara <ayabaara4@gmail.com>
2025-06-01 11:58:35 +0300

    add customerCreationDto model & map it to customer

---
+++ RestaurantReservation.API/Models/CustomerCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.API.Models;
+
+public class CustomerCreationDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
+
--- RestaurantReservation.API/Profiles/CustomerProfile.cs
+++ RestaurantReservation.API/Profiles/CustomerProfile.cs
@@ -9,6 +9,7 @@ public class CustomerProfile : Profile
     public CustomerProfile()
     {
         CreateMap<Customer, CustomerDto>();
+        CreateMap<CustomerCreationDto, Customer>();
     }
 }
 
