678e69d82e889951fcf5235717e47d615eebc4c6
ayabaara <ayabaara4@gmail.com>
2025-06-01 21:05:49 +0300

    add /OrderWithMenuItemsDto model and map it

---
+++ RestaurantReservation.API/Profiles/OrderProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.API.Profiles;
+
+public class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        CreateMap<Order, OrderWithMenuItemsDto>().ForMember(destination => destination.MenuItems, options => options.MapFrom(src => src.OrderItems.Select(i => i.MenuItem)));
+    }
+}
+
