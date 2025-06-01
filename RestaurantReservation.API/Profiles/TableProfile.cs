35fe6ac1f8b13aad1b1b37303c4ecc9997c459fc
ayabaara <ayabaara4@gmail.com>
2025-06-01 17:50:09 +0300

    add table mapper

---
+++ RestaurantReservation.API/Profiles/TableProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.API.Profiles;
+
+public class TableProfile : Profile
+{
+    public TableProfile()
+    {
+        CreateMap<Table, TableDto>();
+        CreateMap<TableCreationDto, Table>();
+    }
+}
+
