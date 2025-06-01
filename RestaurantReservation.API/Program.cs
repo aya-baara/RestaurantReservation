2025deb809b0e1dffd1054e48119037ec8e174e5
ayabaara <ayabaara4@gmail.com>
2025-06-01 11:00:33 +0300

    Add connection string & configure Dbcontext

--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<RestaurantReservationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
